11e45834fbbd04c1c2172b6035381f57abd8b1f7
slojanko <jankec72@gmail.com>
2022-12-01 23:05:22 +0100

    Day 1 solution

---
+++ advent-of-code-2022/Code/day1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Day1
+{
+    private List<int> DeerCalories;
+
+    public Day1()
+    {
+        DeerCalories = new List<int>();
+        DeerCalories.Add(0);
+    }
+
+    public void Solve()
+    {
+        var input = File.ReadAllLines(".\\Inputs\\day1.txt");
+        foreach (var calories in input)
+        {
+            if (int.TryParse(calories, out int c))
+            {
+                DeerCalories[DeerCalories.Count - 1] += c;
+            }
+            else
+            {
+                DeerCalories.Add(0);
+            }
+        }
+
+        DeerCalories.Sort((int a, int b) => b.CompareTo(a));
+        Console.WriteLine(DeerCalories[0]);
+        Console.WriteLine(DeerCalories[0] + DeerCalories[1] + DeerCalories[2]);
+    }
+}
---
+++ advent-of-code-2022/Program.cs
@@ -0,0 +1,8 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Day1 day1 = new Day1();
+        day1.Solve();
+    }
+}
